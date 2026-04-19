7e44f172ba5835a9deff7e015676b72ada47c841
Ubuntu <ubuntu@ip-172-31-41-111.ap-south-1.compute.internal>
2026-04-19 08:53:22 +0000

    fix: terraform fmt for stg

--- environments/stg/main.tf
+++ environments/stg/main.tf
@@ -1,20 +1,20 @@
 module "ec2" {
-  source         = "../../modules/ec2"
-  ami_id         = "ami-0e12ffc2dd465f6e4"
-  instance_type  = "t3.micro"
-  key_name       = "terraform-key"
-  name           = "stg-ec2"
-  env            = "stg"
+  source        = "../../modules/ec2"
+  ami_id        = "ami-0e12ffc2dd465f6e4"
+  instance_type = "t3.micro"
+  key_name      = "terraform-key"
+  name          = "stg-ec2"
+  env           = "stg"
 }
 
 module "s3" {
-  source       = "../../modules/s3"
-  bucket_name  = "my-stg-bucket-18042026"
-  env          = "stg"
+  source      = "../../modules/s3"
+  bucket_name = "my-stg-bucket-18042026"
+  env         = "stg"
 }
 
 module "dynamodb" {
-  source      = "../../modules/dynamodb"
-  table_name  = "stg-table"
-  env         = "stg"
+  source     = "../../modules/dynamodb"
+  table_name = "stg-table"
+  env        = "stg"
 }
